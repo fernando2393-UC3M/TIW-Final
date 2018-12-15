eb97115c4cfe705c76de27154a605133701807d7
Guille Escobero <100346060@alumnos.uc3m.es>
2018-12-15 23:17:13 +0000

    Check check fer

--- TIWbnbAdmin/src/main/AdminServlet.java
+++ TIWbnbAdmin/src/main/AdminServlet.java
@@ -414,7 +414,7 @@ public class AdminServlet extends HttpServlet {
 				result.setHomePriceNight(resultHome.getHomePriceNight());
 			}
 			
-			if(!req.getParameter("inputPhotos").isEmpty()) {
+			if(req.getParameter("inputPhotos") != null) {
 				String pic = "images/"+req.getParameter("inputPhotos");
 				result.setHomePhotos(pic);
 			}
