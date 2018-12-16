15a645b6d2ccfa2b4ebe458d9b4770161fac6cfd
Antonio de Padua Díaz Martínez <tonidiazmartinez1997@gmail.com>
2018-12-16 01:39:56 -0500

    Update BNBServlet.java

--- TIWbnb/src/main/BNBServlet.java
+++ TIWbnb/src/main/BNBServlet.java
@@ -15,6 +15,7 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
